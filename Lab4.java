4a01f7d5f4f6559e98473afff9a3fcb084e8ec3d
samwell013 <samwell.sav@gmail.com>
2017-09-11 21:20:16 +0300

    ‘r15’

--- Lab4.java
+++ Lab4.java
@@ -11,7 +11,7 @@ public class Lab4 {
   b.s36();
   b.s5();
   b.s39();
-  c.s261();
+  c.s26();
   a.s32();
   b.s20();
   c.s22(a);
@@ -19,3 +19,4 @@ public class Lab4 {
   b.s22(c);
   }
 }
+previous : 13
